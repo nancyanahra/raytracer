aebbb5e36782c6589b17281db817df277cfb6450
Nancy Nahra <nnahra@uiowa.edu>
2026-03-17 16:28:22 -0500

    adding gamma correction for accurate color intensity variance

--- color.hpp
+++ color.hpp
@@ -3,11 +3,27 @@
 #include "vec3.hpp"
 #include "interval.hpp"
 using color = vec3;
+
+inline double linear_to_gamma(double linear_component){
+
+    if(linear_component >0){
+        return std::sqrt(linear_component);
+    }
+    
+    return 0;
+
+}
+
 void write_color(std::ostream& out, const color& pixel_color) {
 
     auto r = pixel_color.x();
     auto g = pixel_color.y();
     auto b = pixel_color.z();
+    
+    // apply linear to gamma transform for gamma 2
+    r = linear_to_gamma(r);
+    g = linear_to_gamma(g);
+    b = linear_to_gamma(b);
 
     // Translate the [0,1] component values to the byte range [0,255].
     static const interval intensity(0.000, 0.999);
