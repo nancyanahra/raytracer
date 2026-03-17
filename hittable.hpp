38f2288abe88f7893a46bca771952de8e03f251a
Nancy Nahra <nnahra@uiowa.edu>
2026-03-17 17:37:26 -0500

    creating new data structure to describe ray-object intersections specific to a material type

--- hittable.hpp
+++ hittable.hpp
@@ -1,46 +1,48 @@
 #ifndef HITTABLE_HPP
 #define HITTABLE_HPP
 
+class material;
 
-    class hit_record {
+class hit_record {
 
-	    public: 
-	        point3 p;
-	        vec3 normal;
-	        //our hit will only count if this t is: t_min < t < t_max
-	        double t;
-	        
-	        bool front_face;
-	        
-	        void set_face_normal(const ray& r, const vec3& outward_normal){
-	        
-	            // sets the hit record normal vector
-	            // note: the parameter 'outward_normal' is assumed to have unit length
-	            
-	            // angle > 90 deg , so ray direction and 
-	            //outward surface normal point in opposite directions
-	            // so true if ray is hitting the outside surface of sphere
-	            front_face = dot(r.direction(), outward_normal) < 0;
-	            // normal otherwise points inward towards sphere if ray dir and surf norm 
-	            // point in same direction
-	            normal = front_face ? outward_normal : -outward_normal;
-	            
-	            }
-
-	        
-	        
-	        
-    };
-
-    class hittable {
-
-        public:
-            virtual ~hittable() = default;
+    public: 
+        point3 p;
+        vec3 normal;
+        shared_ptr<material> mat;
+        //our hit will only count if this t is: t_min < t < t_max
+        double t;
+        
+        bool front_face;
+        
+        void set_face_normal(const ray& r, const vec3& outward_normal){
+        
+            // sets the hit record normal vector
+            // note: the parameter 'outward_normal' is assumed to have unit length
             
-            // (abstract base class (interface) to be implemented by any object derived off hittable
-            virtual bool hit(const ray& r, interval ray_t, hit_record& rec) const = 0;
-         
+            // angle > 90 deg , so ray direction and 
+            //outward surface normal point in opposite directions
+            // so true if ray is hitting the outside surface of sphere
+            front_face = dot(r.direction(), outward_normal) < 0;
+            // normal otherwise points inward towards sphere if ray dir and surf norm 
+            // point in same direction
+            normal = front_face ? outward_normal : -outward_normal;
+            
+            }
 
-    };
-    
-    #endif
+        
+        
+        
+};
+
+class hittable {
+
+    public:
+        virtual ~hittable() = default;
+        
+        // (abstract base class (interface) to be implemented by any object derived off hittable
+        virtual bool hit(const ray& r, interval ray_t, hit_record& rec) const = 0;
+     
+
+};
+
+#endif
--- sphere.hpp
+++ sphere.hpp
@@ -8,7 +8,9 @@ class sphere : public hittable {
 
 
     public:
-        sphere(const point3& center, double radius) : center(center), radius(std::fmax(0, radius)) {}
+        sphere(const point3& center, double radius) : center(center), radius(std::fmax(0, radius)) {
+            // TODO: Initialize the material pointer, "mat"
+        }
         
         bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
             // center - origin
@@ -54,6 +56,7 @@ class sphere : public hittable {
 	        // on the sphere FROM the direction of the hit point
             vec3 outward_normal = (rec.p - center) / radius;
             rec.set_face_normal(r, outward_normal);
+            rec.mat = mat;
             
             return true;    
         
@@ -63,6 +66,7 @@ class sphere : public hittable {
      
         point3 center;
         double radius;
+        shared_ptr<material> mat;
 
 };
 
